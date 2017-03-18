273328a9effc03d40bdcddce36ca8e06340ea3f4
Kiran <Kiran@Kodimak-PC>
2017-03-18 19:24:33 +0530

    sample service

---
+++ person/src/main/java/com/person/PersonApplication.java
@@ -0,0 +1,23 @@
+package com.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
